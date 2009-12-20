889220839890f2bbbcd72f6454839a4291fcf462
Dogacan Guney <dogacan@gmail.com>
2009-12-20 16:21:50 +0200

    Merge String serialization into a single file

--- src/java/org/apache/nutch/storage/mapreduce/StringSerialization.java
+++ src/java/org/apache/nutch/storage/mapreduce/StringSerialization.java
@@ -1,5 +1,12 @@
 package org.apache.nutch.storage.mapreduce;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.serializer.Deserializer;
 import org.apache.hadoop.io.serializer.Serialization;
 import org.apache.hadoop.io.serializer.Serializer;
@@ -13,11 +20,47 @@ public class StringSerialization implements Serialization<String> {
 
   @Override
   public Deserializer<String> getDeserializer(Class<String> c) {
-    return null;
+    return new Deserializer<String>() {
+      private DataInputStream in;
+
+      @Override
+      public void open(InputStream in) throws IOException {
+        this.in = new DataInputStream(in);
+      }
+
+      @Override
+      public void close() throws IOException {
+        this.in.close();
+      }
+
+      @Override
+      public String deserialize(String t) throws IOException {
+        return Bytes.toString(Bytes.readByteArray(in));
+      }
+    };
   }
 
   @Override
   public Serializer<String> getSerializer(Class<String> c) {
-    return new StringSerializer();
+    return new Serializer<String>() {
+
+      private DataOutputStream out;
+
+      @Override
+      public void close() throws IOException {
+        this.out.close();
+      }
+
+      @Override
+      public void open(OutputStream out) throws IOException {
+        this.out = new DataOutputStream(out);
+      }
+
+      @Override
+      public void serialize(String t) throws IOException {
+        Bytes.writeByteArray(out, Bytes.toBytes(t));
+      }
+      
+    };
   }
 }
